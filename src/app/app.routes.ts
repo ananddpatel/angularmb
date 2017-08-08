7b21a36751f43d031bfabdea26b8bd97430bf4b0
Anand Patel <anandpatel3@hotmail.com>
2017-08-07 21:18:51 -0400

    post submittion to appropriate board now works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { BoardComponent } from './components/board/board.component';
 import { AuthComponent } from './components/auth/auth.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 
 const routes = [
   {path: '', component: HomeComponent},
   {path: 'auth', component: AuthComponent},
   {path: 'b/:board',component: BoardComponent},
+  {path: 'b/:board/create',component: CreatePostComponent},
   {path: '404', component: NotfoundComponent},
   {path: '**', redirectTo: '404'}
 ];
--- src/app/components/create-post/create-post.component.ts
+++ src/app/components/create-post/create-post.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { Router } from "@angular/router";
+import { DataService } from "../../services/data.service";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-create-post',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-post.component.css']
 })
 export class CreatePostComponent implements OnInit {
+  board: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private data: DataService
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(res => this.board = res.board);
+  }
+
+  createPost(form: NgForm) {
+    this.data.createPost(this.board, form.value.title, form.value.body)
+      .subscribe(res => {
+        console.log(res.statusText);
+        this.router.navigate(['/b/'+this.board])
+      })
   }
 
 }
